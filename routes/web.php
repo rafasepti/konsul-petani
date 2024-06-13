078d82e85276e8357055048398e8a8503a71de1f
rafasepti <rafasepti@gmail.com>
2024-06-14 06:43:25 +0700

    add: middleware role

--- routes/web.php
+++ routes/web.php
@@ -39,48 +39,48 @@ Route::get('/dashboard', function () {
         'jumlah_pertanyaan' => $jumlah_pertanyaan,  
         'jumlah_penyakit' => $jumlah_penyakit,  
     ]);
-})->middleware(['auth'])->name('dashboard');
+})->middleware(['auth', 'checkRole:admin'])->name('dashboard');
 
 // untuk crud user
-Route::get('/user','App\Http\Controllers\UserController@index')->middleware(['auth'])->name('user');
-Route::get('/user/create', 'App\Http\Controllers\UserController@create')->middleware(['auth']);
-Route::post('/user/store', 'App\Http\Controllers\UserController@store')->middleware(['auth']);
-Route::get('/user/destroy/{id}', '\App\Http\Controllers\UserController@destroy')->middleware(['auth']);
-Route::get('/user/edit/{id}', 'App\Http\Controllers\UserController@edit')->middleware(['auth']);
-Route::post('/user/update', 'App\Http\Controllers\UserController@update')->middleware(['auth']);
+Route::get('/user','App\Http\Controllers\UserController@index')->middleware(['auth', 'checkRole:admin'])->name('user');
+Route::get('/user/create', 'App\Http\Controllers\UserController@create')->middleware(['auth', 'checkRole:admin']);
+Route::post('/user/store', 'App\Http\Controllers\UserController@store')->middleware(['auth', 'checkRole:admin']);
+Route::get('/user/destroy/{id}', '\App\Http\Controllers\UserController@destroy')->middleware(['auth', 'checkRole:admin']);
+Route::get('/user/edit/{id}', 'App\Http\Controllers\UserController@edit')->middleware(['auth', 'checkRole:admin']);
+Route::post('/user/update', 'App\Http\Controllers\UserController@update')->middleware(['auth', 'checkRole:admin']);
 
 // untuk crud gejala
-Route::get('/gejala','App\Http\Controllers\GejalaController@index')->middleware(['auth'])->name('gejala');
-Route::get('/gejala/create', 'App\Http\Controllers\GejalaController@create')->middleware(['auth']);
-Route::post('/gejala/store', 'App\Http\Controllers\GejalaController@store')->middleware(['auth']);
-Route::get('/gejala/destroy/{id}', '\App\Http\Controllers\GejalaController@destroy')->middleware(['auth']);
-Route::get('/gejala/edit/{id}', 'App\Http\Controllers\GejalaController@edit')->middleware(['auth']);
-Route::post('/gejala/update', 'App\Http\Controllers\GejalaController@update')->middleware(['auth']);
+Route::get('/gejala','App\Http\Controllers\GejalaController@index')->middleware(['auth', 'checkRole:admin'])->name('gejala');
+Route::get('/gejala/create', 'App\Http\Controllers\GejalaController@create')->middleware(['auth', 'checkRole:admin']);
+Route::post('/gejala/store', 'App\Http\Controllers\GejalaController@store')->middleware(['auth', 'checkRole:admin']);
+Route::get('/gejala/destroy/{id}', '\App\Http\Controllers\GejalaController@destroy')->middleware(['auth', 'checkRole:admin']);
+Route::get('/gejala/edit/{id}', 'App\Http\Controllers\GejalaController@edit')->middleware(['auth', 'checkRole:admin']);
+Route::post('/gejala/update', 'App\Http\Controllers\GejalaController@update')->middleware(['auth', 'checkRole:admin']);
 
 // untuk crud penyakit
-Route::get('/penyakit','App\Http\Controllers\PenyakitController@index')->middleware(['auth'])->name('penyakit');
-Route::get('/penyakit/create', 'App\Http\Controllers\PenyakitController@create')->middleware(['auth']);
-Route::post('/penyakit/store', 'App\Http\Controllers\PenyakitController@store')->middleware(['auth']);
-Route::get('/penyakit/destroy/{id}', '\App\Http\Controllers\PenyakitController@destroy')->middleware(['auth']);
-Route::get('/penyakit/edit/{id}', 'App\Http\Controllers\PenyakitController@edit')->middleware(['auth']);
-Route::post('/penyakit/update', 'App\Http\Controllers\PenyakitController@update')->middleware(['auth']);
+Route::get('/penyakit','App\Http\Controllers\PenyakitController@index')->middleware(['auth', 'checkRole:admin'])->name('penyakit');
+Route::get('/penyakit/create', 'App\Http\Controllers\PenyakitController@create')->middleware(['auth', 'checkRole:admin']);
+Route::post('/penyakit/store', 'App\Http\Controllers\PenyakitController@store')->middleware(['auth', 'checkRole:admin']);
+Route::get('/penyakit/destroy/{id}', '\App\Http\Controllers\PenyakitController@destroy')->middleware(['auth', 'checkRole:admin']);
+Route::get('/penyakit/edit/{id}', 'App\Http\Controllers\PenyakitController@edit')->middleware(['auth', 'checkRole:admin']);
+Route::post('/penyakit/update', 'App\Http\Controllers\PenyakitController@update')->middleware(['auth', 'checkRole:admin']);
 
 // untuk crud penyakit solusi
-Route::get('/penyakitsolusi','App\Http\Controllers\PenyakitSolusiController@index')->middleware(['auth'])->name('penyakitsolusi');
-Route::get('/penyakitsolusi/create', 'App\Http\Controllers\PenyakitSolusiController@create')->middleware(['auth']);
-Route::post('/penyakitsolusi/store', 'App\Http\Controllers\PenyakitSolusiController@store')->middleware(['auth']);
-Route::get('/penyakitsolusi/destroy/{id}', '\App\Http\Controllers\PenyakitSolusiController@destroy')->middleware(['auth']);
-Route::get('/penyakitsolusi/edit/{id}', 'App\Http\Controllers\PenyakitSolusiController@edit')->middleware(['auth']);
-Route::post('/penyakitsolusi/update', 'App\Http\Controllers\PenyakitSolusiController@update')->middleware(['auth']);
+Route::get('/penyakitsolusi','App\Http\Controllers\PenyakitSolusiController@index')->middleware(['auth', 'checkRole:admin'])->name('penyakitsolusi');
+Route::get('/penyakitsolusi/create', 'App\Http\Controllers\PenyakitSolusiController@create')->middleware(['auth', 'checkRole:admin']);
+Route::post('/penyakitsolusi/store', 'App\Http\Controllers\PenyakitSolusiController@store')->middleware(['auth', 'checkRole:admin']);
+Route::get('/penyakitsolusi/destroy/{id}', '\App\Http\Controllers\PenyakitSolusiController@destroy')->middleware(['auth', 'checkRole:admin']);
+Route::get('/penyakitsolusi/edit/{id}', 'App\Http\Controllers\PenyakitSolusiController@edit')->middleware(['auth', 'checkRole:admin']);
+Route::post('/penyakitsolusi/update', 'App\Http\Controllers\PenyakitSolusiController@update')->middleware(['auth', 'checkRole:admin']);
 
 // untuk crud pertanyaan
-Route::get('/pertanyaan','App\Http\Controllers\PertanyaanController@index')->middleware(['auth'])->name('pertanyaan');
-Route::get('/pertanyaan/create', 'App\Http\Controllers\PertanyaanController@create')->middleware(['auth']);
-Route::post('/pertanyaan/store', 'App\Http\Controllers\PertanyaanController@store')->middleware(['auth']);
-Route::get('/pertanyaan/destroy/{id}', '\App\Http\Controllers\PertanyaanController@destroy')->middleware(['auth']);
-Route::get('/pertanyaan/edit/{id}', 'App\Http\Controllers\PertanyaanController@edit')->middleware(['auth']);
-Route::post('/pertanyaan/update', 'App\Http\Controllers\PertanyaanController@update')->middleware(['auth']);
+Route::get('/pertanyaan','App\Http\Controllers\PertanyaanController@index')->middleware(['auth', 'checkRole:admin'])->name('pertanyaan');
+Route::get('/pertanyaan/create', 'App\Http\Controllers\PertanyaanController@create')->middleware(['auth', 'checkRole:admin']);
+Route::post('/pertanyaan/store', 'App\Http\Controllers\PertanyaanController@store')->middleware(['auth', 'checkRole:admin']);
+Route::get('/pertanyaan/destroy/{id}', '\App\Http\Controllers\PertanyaanController@destroy')->middleware(['auth', 'checkRole:admin']);
+Route::get('/pertanyaan/edit/{id}', 'App\Http\Controllers\PertanyaanController@edit')->middleware(['auth', 'checkRole:admin']);
+Route::post('/pertanyaan/update', 'App\Http\Controllers\PertanyaanController@update')->middleware(['auth', 'checkRole:admin']);
 
-Route::get('/konsultasi','App\Http\Controllers\PertanyaanController@konsultasi')->middleware(['auth'])->name('konsultasi');
+Route::get('/konsultasi','App\Http\Controllers\PertanyaanController@konsultasi')->middleware(['auth', 'checkRole:admin'])->name('konsultasi');
 
 require __DIR__.'/auth.php';
